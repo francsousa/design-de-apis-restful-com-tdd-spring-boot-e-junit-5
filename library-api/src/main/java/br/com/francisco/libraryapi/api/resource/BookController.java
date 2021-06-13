486e4015621227f03e9163bb12c074baf12346ad
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-06-13 15:49:58 -0300

    Controller, DTO e Lombok Project

---
+++ library-api/src/main/java/br/com/francisco/libraryapi/api/resource/BookController.java
@@ -0,0 +1,28 @@
+package br.com.francisco.libraryapi.api.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.francisco.libraryapi.api.dto.BookDto;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public BookDto create() {
+		
+		BookDto dto = new BookDto();
+		
+		dto.setAuthor("Autor");
+		dto.setTitle("Meu Livro");
+		dto.setIsbn("1213212");
+		dto.setId(1L);
+		
+		return dto;
+	}
+}
