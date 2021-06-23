be5069db944760f14e5a7c75741b453632c3fa60
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-06-23 07:55:07 -0300

    Implementando a consulta de livros

--- library-api/src/main/java/br/com/francisco/libraryapi/api/resource/BookController.java
+++ library-api/src/main/java/br/com/francisco/libraryapi/api/resource/BookController.java
@@ -1,8 +1,14 @@
 package br.com.francisco.libraryapi.api.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -68,6 +74,18 @@ public class BookController {
 			return modelMapper.map(book, BookDto.class);
 		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 		
+	}
+	
+	@GetMapping
+	public Page<BookDto> find(BookDto dto, Pageable pageRequest) {
+		Book filter = modelMapper.map(dto, Book.class);
+		
+		Page<Book> result = service.find(filter, pageRequest);
+		List<BookDto> list = result.getContent().stream()
+			.map(entity -> modelMapper.map(entity, BookDto.class))
+			.collect(Collectors.toList());
+		
+		return new PageImpl<BookDto>(list, pageRequest, result.getTotalElements());
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
